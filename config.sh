cf6eaf72f9f8300a88d599ebeeb0ab6ee273ca2c
river <river@gmail.com>
2023-08-01 10:19:16 +0800

    chore: add apk into config.sh

--- config.sh
+++ config.sh
@@ -1,5 +1,29 @@
 #!/usr/bin/env bash
 
+vim_depd(){
 apk add \
 		vifm \
-		just
+		just \
+		clang \
+		clang16-extra-tools
+# clangd
+apk add python3 py3-pip
+pip install clangd
+}
+
+alpine_add(){
+apk add \
+	build-base \
+	vim \
+	git \
+	curl
+}
+
+fish_config(){
+apk add \
+	fish
+ohmyfish="https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/master/bin/install"
+curl "$ohmyfish" | fish
+omf install ays
+}
+
