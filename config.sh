9b0a9c3b96d6240a766db2e769170d7111f5758f
tower_town <FibonacciNuture@foxmail.com>
2023-08-05 08:35:44 +0800

    chore(config): update config.sh for Alpine init

--- config.sh
+++ config.sh
@@ -5,7 +5,7 @@
 #   Author        : tower_town
 #   Email         : tower_town@outlook.com
 #   File Name     : config.sh
-#   Last Modified : 2023-08-04 10:22
+#   Last Modified : 2023-08-05 08:34
 #   Describe      : 
 #
 # ====================================================
@@ -31,12 +31,37 @@ npm install -g vim-language-server
 npm prefix -g vim-language-server
 }
 
-alpine_add(){
+alpine_init(){
+# setting mirror apk rep
+echo "setting mirror apk rep"
+sed -i 's/dl-cdn.alpinelinux.org/mirrors.tuna.tsinghua.edu.cn/g' /etc/apk/repositories
+
+# basic apk
+echo "install basic apk"
 apk add \
 	build-base \
 	vim \
 	git \
-	curl
+	curl \
+	less
+
+# install docs reference: wiki.alpinelinux.com/wiki/Alpine_Linux:FAQ
+echo "install docs reference: wiki.alpinelinux.com/wiki/Alpine_Linux:FAQ"
+apk add mandoc man-pages docs
+
+# setting timezone reference: wiki.alpinelinux.com/wiki/Setting_the_timezone
+echo "setting timezone."
+apk add tzdata
+ls /usr/share/zoneinfo
+
+# setting timezone to Asia/Shanghai
+cp /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
+
+echo "Asia/Shanghai" > /etc/timezone
+date
+
+apk del tzdata
+echo "setting timezone finish."
 }
 
 fish_config(){
